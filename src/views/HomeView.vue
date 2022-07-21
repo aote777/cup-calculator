c168edd239c0ec69200c70763ae5be1d6a35bc2e
aote777 <1540464777@qq.com>
2022-07-21 10:28:35 +0800

    cccc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="home">
+    <h6>计算在本地完成,不收集任何数据</h6>
+    <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+      <el-tab-pane label="计算" name="first">User</el-tab-pane>
+      <el-tab-pane label="反推" name="second">Config</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-  },
-});
+const activeName = ref('first')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+  console.log(tab, event)
+}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = '罩杯计算器'
+      return args
+    })
+  },
 })
